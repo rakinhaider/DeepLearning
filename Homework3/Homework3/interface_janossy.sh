a419a686723b8e331346f5dc0643ad06045afab6
Chowdhury Mohammad Rakin Haider <chaider@scholar-fe04.rcac.purdue.edu>
2021-03-30 19:42:33 -0400

    interfaces changed in scholar.

--- Homework3/Homework3/interface_janossy.sh
+++ Homework3/Homework3/interface_janossy.sh
@@ -8,7 +8,7 @@
 # It will automatically generate `sbatch` submission file and submit, so you
 # do not need to write submission commands by yourself.
 # To run on GPU, replace device variable from "cpu" to "cuda".
-sbatch="--sbatch"
+sbatch=""
 student="template"
 datrot="../Data"
 seed="0"
@@ -21,12 +21,14 @@ epochs="100"
 
 
 # 2-layer Sparse Janossy.
-python main.py --cora ${sbatch} --student ${student} \
-    --data ${datrot}/Cora --random-seed ${seed} \
-    --batch-size -1 --truncate 0 --num-workers ${workers} \
-    --num-internals 16 --num-graph-layers 2 --num-perms 1 --janossy \
-    --learning-rate 0.01 --weight-decay 5e-5 --clip inf \
-    --num-epochs ${epochs} --device ${device}
+# python main.py --cora ${sbatch} --student ${student} \
+#     --data ${datrot}/Cora --random-seed ${seed} \
+#     --batch-size -1 --truncate 0 --num-workers ${workers} \
+#     --num-internals 16 --num-graph-layers 2 --num-perms 1 --janossy \
+#     --learning-rate 0.01 --weight-decay 5e-5 --clip inf \
+#     --num-epochs ${epochs} --device ${device}
+
+epochs="2"
 
 # 2-layer Sparse Janossy with 20 permutations.
 python main.py --cora ${sbatch} --student ${student} \
